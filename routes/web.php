f1b1066a6ffc13ef975b71930cfb3bc7f3fdc02a
teyfikaktas <aktasteyf@gmail.com>
2025-07-02 00:05:28 +0300

    job detail

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
+use App\Livewire\JobDetail;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', HomePage::class);
+Route::get('/jobs/{id}', JobDetail::class)->name('job.detail');
